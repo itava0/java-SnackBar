c979295c2361cb9e7b88500db6b70f451228c6e7
Ivan Tavarez <40846384+itava0@users.noreply.github.com>
2019-12-02 15:38:51 -0500

    Praticed class

---
+++ src/snack.java
@@ -0,0 +1,17 @@
+package snack
+static int car = 5;
+
+public static void main (String [] args) {
+    int count = 0;
+        {
+            int coffee = 5;
+            int car = 15;
+            System.out.println(car);
+            System.out.println(count++);
+        }
+        System.out.println(count);
+        {
+            int coffee = 10;
+        }
+        System.out.println(car);
+        }
